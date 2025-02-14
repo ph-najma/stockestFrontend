873c3f934a8d58b8d9ecc4e26429aa75dae07653
Najma <phnajma786@gmail.com>
2025-02-14 09:20:48 +0530

    made changes in the environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   apiUrl: '13.202.29.25:5000',
   appName: 'My Angular App',
+  googleClientId:
+    '89186691123-vmrq99r21diqv7lhmrju7vkho7qbdcdo.apps.googleusercontent.com',
 };
