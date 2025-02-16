93241b4299f92b6e4f35831f26c99aa93d50201e
Najma <phnajma786@gmail.com>
2025-02-16 10:34:53 +0530

    chamged the production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '13.202.29.25:5000',
+  apiUrl: '13.233.40.98:5000',
   appName: 'My Angular App',
   googleClientId:
     '89186691123-vmrq99r21diqv7lhmrju7vkho7qbdcdo.apps.googleusercontent.com',
